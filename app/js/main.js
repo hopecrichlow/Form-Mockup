a4f090039211a03375a0ba0cf9cf7d4c587489e9
Jontae Pierce <AirForcePierce@users.noreply.github.com>
2015-10-13 21:57:06 -0400

    adding select and functions that spit out values needed

--- app/js/main.js
+++ app/js/main.js
@@ -16,8 +16,19 @@
   //Template
 
   var textInput = function textInput(obj) {
-    var template = '\n    <div class="text-input">\n    <input value="" type="' + obj.type + '" placeholder="' + obj.label + '" id="' + obj.id + '">\n    <i class="fa ' + obj.icon + '"></i>\n    <""\n\n    </div>\n    ';
+    var template = '\n    <div class="text-input">\n    <input type="' + obj.type + '" placeholder="' + obj.label + '" id="' + obj.id + '">\n    </div>\n    ';
     return template;
+  };
+
+  var selectInput = function selectInput(obj) {
+    var addedOptions = '';
+
+    obj.options.forEach(function (options) {
+      addedOptions += '<option value="' + options.value + '">' + options.label + '</option>';
+    });
+
+    var select = '\n    <div class="select-input">\n    <select type="' + obj.type + '" placeholder= "' + obj.label + '" id="' + obj.id + '">\n    <option value="">Select Language...</option>\n      ' + addedOptions + ' \n    </select>\n    </div>\n    ';
+    return select;
   };
 
   //Our function to use data
@@ -27,8 +38,10 @@
     _.each(arr, function (item) {
       var htmlBlock;
 
-      if (item.type === 'text' || item.type === 'tel' || item.type === 'email') {
+      if (item.type === 'text' || item.type === 'tel' || item.type === 'email' || item.type === 'textarea') {
         htmlBlock = textInput(item);
+      } else if (item.type === 'select') {
+        htmlBlock = selectInput(item);
       }
 
       $('form').append(htmlBlock);
